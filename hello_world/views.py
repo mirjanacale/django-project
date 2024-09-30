05a3229e7895d7f673e7c13e43b3fd48d36109b2
Mirjana Cale <mirjana083@gmail.com>
2024-09-30 20:07:03 +0000

    Adjust the view so that if the request method is POST

--- hello_world/views.py
+++ hello_world/views.py
@@ -3,5 +3,10 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello,world")
+    if request.method == "POST":
+        return HttpResponse("You must have POSTed something")
+    else:
+        return HttpResponse(request.method)
+    
+   
     
